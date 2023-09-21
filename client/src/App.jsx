98e9af5785ca5948fc2650669b8d8b9ffdc1f7df
anlety <79236211+anlety@users.noreply.github.com>
2023-09-21 10:29:12 +1000

    set the react-router-dom in the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,27 @@
 
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
 
 
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
